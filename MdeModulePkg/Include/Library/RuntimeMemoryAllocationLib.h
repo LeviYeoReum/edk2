e3704af80eba6a214b09c93fe7fcd5f9024b2ce9
Levi Yun <yeoreum.yun@arm.com>
2025-07-18 15:37:45 +0100

    MdeModulePkg/Library: introduce RuntimeMemoryAllocationLib
    Not only BaseCryptoLib, FmpDxe needs dynamic memory allocation to
    support runtime capsule update.
    
    For this, make a RuntimeMemoryAllocationLib with
    core implementation of
    CryptoPkg\Library\BaseCryptLib\Syscall\RuntimeMemAllocation.c
    
    Signed-off-by: Yeoreum Yun <yeoreum.yun@arm.com>

---
+++ MdeModulePkg/Include/Library/RuntimeMemoryAllocationLib.h
@@ -0,0 +1,54 @@
+/** @file
+  Light-weight Memory Management Routines at Runtime Phase.
+
+  Copyright (c) 2025, Arm Ltd. All rights reserved.<BR>
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+ **/
+
+#ifndef RUNTIME_MEMORY_ALLOCATION_LIB_H_
+#define RUNTIME_MEMORY_ALLOCATION_LIB_H_
+
+/**
+  Allocates a buffer at runtime phase.
+
+  @param[in]  AllocationSize    Bytes to be allocated.
+
+  @return  A pointer to the allocated buffer or NULL if allocation fails.
+
+ **/
+VOID *
+EFIAPI
+RuntimeAllocateMem (
+  IN  UINTN  AllocationSize
+  );
+
+/**
+  Reallocates a buffer at runtime phase.
+
+  @param[in]  OldPtr            Origin allocated memory.
+  @param[in]  Size              Bytes to be allocated.
+
+  @return  A pointer to the allocated buffer or NULL if allocation fails.
+
+ **/
+VOID *
+EFIAPI
+RuntimeReallocateMem (
+  IN  VOID   *OldPtr,
+  IN  UINTN  Size
+  );
+
+/**
+  Frees a buffer that was previously allocated at runtime phase.
+
+  @param[in]  Buffer  Pointer to the buffer to free.
+
+ **/
+VOID
+EFIAPI
+RuntimeFreeMem (
+  IN  VOID  *Buffer
+  );
+
+#endif
